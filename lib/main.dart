d7343878ac45a02db7f148a3717dace3733487b5
Ameer Hamza <Ameerhamza4207@gmail.com>
2024-08-11 15:33:30 +0500

    Home Code is cheched In

--- lib/main.dart
+++ lib/main.dart
@@ -15,7 +15,7 @@ class MyApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return MaterialApp(
       debugShowCheckedModeBanner: false,
-      title: 'Flutter Demo',
+      title: 'Hamza APP',
       theme: ThemeData(
         colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
         useMaterial3: true,
@@ -24,4 +24,3 @@ class MyApp extends StatelessWidget {
     );
   }
 }
-
